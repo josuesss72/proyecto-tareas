7f191cc5a914a1e6176568260e5a1d279f2bbb77
josuesss72 <jjdanielsarmiento@gmail.com>
2025-08-09 14:07:53 -0500

    feat: se permite actualizar el status de la task

--- server/src/task/dto/update-task.dto.ts
+++ server/src/task/dto/update-task.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
+import { IsEnum, IsOptional } from 'class-validator';
+import { TaskStatus } from '@prisma/client';
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+  @IsEnum(TaskStatus)
+  @IsOptional()
+  status?: TaskStatus;
+}
